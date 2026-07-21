60a94a457f82eb3beba66ba5f66bb4e949b86930
si-zero <snm306@naver.com>
2026-07-21 15:23:57 +0900

    chore: 채팅 모듈에 인증 공통 모듈 연결

--- src/domains/chat/chat.module.ts
+++ src/domains/chat/chat.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { AuthCommonModule } from '../auth/common/auth-common.module';
+import { ChatAuthenticatedUserService } from './chat-authenticated-user.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 
 @Module({
+  imports: [AuthCommonModule],
   controllers: [ChatController],
-  providers: [ChatGateway, ChatService],
+  providers: [ChatGateway, ChatService, ChatAuthenticatedUserService],
 })
 export class ChatModule {}
