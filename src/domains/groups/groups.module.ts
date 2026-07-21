3912d7fa62d9a010f4e7910e64d52a9838ddce01
Youwol <153346797+si-zero@users.noreply.github.com>
2026-07-21 15:22:37 +0900

    feat: 그룹(GROUP) 도메인 Cognito 인증 리팩토링 (#101)

--- src/domains/groups/groups.module.ts
+++ src/domains/groups/groups.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { AuthCommonModule } from '../auth/common/auth-common.module';
+import { GroupsAuthenticatedUserService } from './groups-authenticated-user.service';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 
 @Module({
+  imports: [AuthCommonModule],
   controllers: [GroupsController],
-  providers: [GroupsService],
+  providers: [GroupsService, GroupsAuthenticatedUserService],
 })
 export class GroupsModule {}
