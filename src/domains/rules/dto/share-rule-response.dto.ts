389278b09ce225290a9a653863c893fe37556192
황은우 <eunuhwang01@gmail.com>
2026-07-25 12:18:29 +0900

    Revert "fix: 규칙(RULE) 메신저 공유 API 구현 (#83)"
    This reverts commit 294912e23382e584aac24ec7411ad85fbb1f5fdd.

--- src/domains/rules/dto/share-rule-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ShareRuleResponseDto {
-  @ApiProperty({ example: 123, description: '공유한 생활 규칙 ID' })
-  ruleId!: number;
-
-  @ApiProperty({ example: 456, description: '생성된 채팅 메시지 ID' })
-  messageId!: number;
-}
